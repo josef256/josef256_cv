27451a94a4483556438f9a0e631a9c0cc8a6b312
Joseph _ <youssef.hanchaoui256@gmail.com>
2020-10-30 10:46:47 +0100

    add link to cv

--- src/components/Home.js
+++ src/components/Home.js
@@ -23,7 +23,7 @@ applications aligned to customer's services. Translating solutions into code and
 working across many different APIs, third-party integrations and databases.
  </div>
                 <div className="flex flex-col md:flex-row items-center">
-                <Link className="w-full md:w-56 mb-4 md:mr-12 md:mb-0" to={'/josef256_cv/assets/Youssef-Hanchaoui_english_lts.pdf'} target="_blank" download>
+                <Link className="w-full md:w-56 mb-4 md:mr-12 md:mb-0" to={'../assets/Youssef-Hanchaoui_english_lts.pdf'} target="_blank" download>
                 <button className="p-1 w-full focus:outline-none  font-medium text-sm text-white bg-blue-500 hover:bg-blue-600 rounded-md mr-6 shadow-lg">
                     Download CV
                 </button>
